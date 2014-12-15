8b8c8c7036e59810590364fabf3ab1a2f230299d
Anthony Camboni <anthony@camboni.be>
2014-12-15 16:20:46 +0100

    Reformat code, add watch option and complete README.md

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # 6to5-nw
+
+A module to develop node-webkit applications with ES6 easily.
+
+## Installation
+As any dependencies of your node-webkit app, just run :
+
+    npm install 6to5-nw --save
+
+## Usage
+
+In your index.html file, you can add a script which call the module
+
+#### require('6to5-nw')(entryScript, options);
+
+`entryScript` is the entry point of your application.
+
+`options` is en object composed by up to 4 elements: 
+  - `browserify` : browserify options object.
+  - `to5` : 6to5 options object.
+  - `polyfill` : `true` or `false` depending if you want the polyfills to be included or not.
+  - `watch` : `true` or `false` depending if you want the application to reload when you save a (client) script or not.
+
+## Example
+
+```html
+<script>
+  require('6to5-nw')('./index', {
+  	browserify: {
+  		debug: true
+  	},
+  	to5: {
+  		sourceMap: 'inline'
+  	},
+  	polyfill: true,
+  	watch: true
+  });
+</script>
+```
--- index.js
+++ index.js
@@ -6,44 +6,49 @@ var watchify = require('watchify');
 var exorcist = require('exorcist');
 
 function run(entryFile, options) {
-	options = options || {};
-	var brOptions = options.browserify || {};
-	var to5Options = options.to5 || {};
+    var brOptions, to5Options, b;
+    options = options || {};
+    brOptions = options.browserify || {};
+    to5Options = options.to5 || {};
 
-	global.watchifyCache = global.watchifyCache || {};
-	global.watchifyPackageCache = global.watchifyPackageCache || {};
+    global.watchifyCache = global.watchifyCache || {};
+    global.watchifyPackageCache = global.watchifyPackageCache || {};
 
-	brOptions.cache = global.watchifyCache;
-	brOptions.packageCache = global.watchifyPackageCache;
-	brOptions.fullPaths = true;	
+    brOptions.cache = global.watchifyCache;
+    brOptions.packageCache = global.watchifyPackageCache;
+    brOptions.fullPaths = true;
 
-	var b = browserify(brOptions);
-	watchify(b)
-	.add(entryFile)
-	.transform(to5ify.configure(to5Options))
-	.on('bundle', function(bundle) {
-		var stream = fs.createWriteStream('./bundle.js');
-		stream.on('finish', function() {
-			if(options.polyfill) addScript(require.resolve('6to5/browser-polyfill'));
-			addScript('bundle.js');
-		});
-		stream.write((options.polyfill ? 'require("6to5/polyfill");' : '') + 'require("6to5/register")(' + JSON.stringify(to5Options) + ');global.requireNode = window.requireNode = require;');
-		bundle.pipe(exorcist('bundle.js.map')).pipe(stream);
-	})
-	.on('update', function() {
-		global.window.location.reload();
-	})
-	.on('log', function(message) {
-		console.log('[watchify]', message)
-	})
-	.bundle();
+    b = browserify(brOptions);
+    watchify(b)
+    .add(entryFile)
+    .transform(to5ify.configure(to5Options))
+    .on('bundle', function(bundle) {
+        var stream = fs.createWriteStream('bundle.js');
+        stream.on('finish', function() {
+            if(options.polyfill) {
+                addScript(require.resolve('6to5/browser-polyfill'));
+            }
+            addScript('bundle.js');
+        });
+        stream.write((options.polyfill ? 'require("6to5/polyfill");' : '') + 'require("6to5/register")(' + JSON.stringify(to5Options) + ');global.requireNode = window.requireNode = require;');
+        bundle.pipe(exorcist('bundle.js.map')).pipe(stream);
+    })
+    .on('update', function() {
+        if(options.watch) {
+            global.window.location.reload();
+        }
+    })
+    .on('log', function(message) {
+        console.log('[watchify]', message);
+    })
+    .bundle();
 }
 
-function addScript(file) {	
-	var doc = global.window.document;
-	var script = doc.createElement('SCRIPT');
-	script.src = file;
-	doc.body.appendChild(script);
+function addScript(file) {
+    var doc = global.window.document,
+        script = doc.createElement('SCRIPT');
+    script.src = file;
+    doc.body.appendChild(script);
 }
 
 module.exports = run;
